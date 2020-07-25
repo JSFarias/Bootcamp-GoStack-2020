18c5a013a2b949fc94e153c5aadc61d63b0d7184
JhonatasFarias <jhonatas.s.farias@gmail.com>
2020-07-24 23:55:18 -0300

    typeORM

---
+++ Nivel02-06/_Projeto-GoBarber/backend02/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import Routes from './routes'
+
+import './database'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(Routes)
+
+app.listen(3333, ()=>{
+  console.log('server started on port 3333')
+})

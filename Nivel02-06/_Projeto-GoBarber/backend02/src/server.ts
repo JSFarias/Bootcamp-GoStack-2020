b7e7aecfabfd9ea7a10513c778d1efe904ffdc77
JhonatasFarias <jhonatas.s.farias@gmail.com>
2020-09-17 20:09:16 -0300

    updating avatar

--- Nivel02-06/_Projeto-GoBarber/backend02/src/server.ts
+++ Nivel02-06/_Projeto-GoBarber/backend02/src/server.ts
@@ -6,10 +6,12 @@ import Routes from './routes'
 
 import './database'
 
+import uploadConfig from './config/upload'
+
 const app = express()
 
 app.use(express.json())
-
+app.use('/files', express.static(uploadConfig.directory))
 app.use(Routes)
 
 app.listen(3333, ()=>{

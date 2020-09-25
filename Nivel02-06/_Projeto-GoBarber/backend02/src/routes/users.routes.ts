bd9a33d7679274de25bb75634f40de19abca6d4a
JhonatasFarias <jhonatas.s.farias@gmail.com>
2020-09-25 19:23:02 -0300

    github-explore react spa started

--- Nivel02-06/_Projeto-GoBarber/backend02/src/routes/users.routes.ts
+++ Nivel02-06/_Projeto-GoBarber/backend02/src/routes/users.routes.ts
@@ -31,22 +31,19 @@ usersRoutes.post('/', async (request, response)=>{
 
 usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response)=>{
   
-  try{
 
-    const updateUserAvatarService = new UpdateUserAvatarService()
-    
-    const user = await updateUserAvatarService.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    })
+  const updateUserAvatarService = new UpdateUserAvatarService()
+  
+  const user = await updateUserAvatarService.execute({
+    user_id: request.user.id,
+    avatarFilename: request.file.filename,
+  })
 
-    delete user.password
+  delete user.password
 
-    return response.json(user)
+  return response.json(user)
 
-  }catch(err){
-    return response.status(400).json({error: err.message})
-  }
+
 
 })
 
--- Nivel02-06/_Projeto-GoBarber/backend02/src/services/CreateAppointmentService.ts
+++ Nivel02-06/_Projeto-GoBarber/backend02/src/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import {getCustomRepository} from 'typeorm'
 
 import Appointment from '../models/Appointments'
 import AppointmentRepository from '../repositories/AppointmentsRepository'
+import AppError from '../errors/AppError'
+
 
 
 interface RequestDTO{
@@ -22,7 +24,7 @@ class CreateAppointmentService{
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate)
     
     if(findAppointmentInSameDate){
-      throw Error('This appointment is already booked')      
+      throw new AppError('This appointment is already booked')      
     } 
 
     const appointment = appointmentsRepository.create({
